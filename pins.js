34ac49ffa4271c5282baa3fd6d35aa9cad07c68e
Andres Perez <eltoroit@users.noreply.github.com>
2021-05-16 16:31:18 -0400

    Update pins.js

--- pins.js
+++ pins.js
@@ -1,52 +1,5 @@
 const rpio = require('rpio');
 
-/*
-Physical	Full Name	WPi	BCM	Name	Other Names (1)	Other Names (2)
-1	3.3v	null	null	3_3v		
-2	5v	null	null	5v		
-3	SDA.1	8	2		SDA_1	
-4	5v	null	null	5v		
-5	SCL.1	9	3		SCL_1	
-6	0v	null	null	0v		
-7	GPIO.4	7	4	GPIO_4		
-8	GPIO.14/TxD	15	14	GPIO_14	TxD	
-9	0v	null	null	0v		
-10	GPIO.15/RxD	16	15	GPIO_15	RxD	
-11	GPIO.17/ce1	0	17	GPIO_17	ce1	
-12	GPIO.18/ce0	1	18	GPIO_18	ce0	
-13	GPIO.27	2	27	GPIO_27		
-14	0v	null	null	0v		
-15	GPIO.22	3	22	GPIO_22		
-16	GPIO.23	4	23	GPIO_23		
-17	3.3v	null	null	3_3v		
-18	GPIO.24	5	24	GPIO_24		
-19	GPIO.10/MOSI	12	10	GPIO_10	MOSI	
-20	0v	null	null	0v		
-21	GPIO.9/MISO	13	9	GPIO_9	MISO	
-22	GPIO.25	6	25	GPIO_25		
-23	GPIO.11/SCLK	14	11	GPIO_11	SCLK	
-24	GPIO.8/CE0	10	8	GPIO_8	CE0	
-25	0v	null	null	0v		
-26	GPIO.7/CE1	11	7	GPIO_7	CE1	
-27	GPIO.0/SDA0/ID_SD	30	0	GPIO_0	SDA0	ID_SD
-28	GPIO.1/SCL0/ID_SC	31	1	GPIO_1	SCL0	ID_SC
-29	GPIO.5	21	5	GPIO_5		
-30	0v	null	null	0v		
-31	GPIO.6	22	6	GPIO_6		
-32	GPIO.12	26	12	GPIO_12		
-33	GPIO.13	23	13	GPIO_13		
-34	0v	null	null	0v		
-35	GPIO.19/miso	24	19	GPIO_19	miso	
-36	GPIO.16/ce2	27	16	GPIO_16	ce2	
-37	GPIO.26	25	26	GPIO_26		
-38	GPIO.20/mosi	28	20	GPIO_20	mosi	
-39	0v	null	null	0v		
-40	GPIO.21/sclk	29	21	GPIO_21	sclk	
-*/
-// This table was generated from:
-// - $ gpio readall (but the names are a bit outdated)
-// - $ pinout (which is more accurate)
-// http://abyz.me.uk/rpi/pigpio/#Type_3
 const PINS = [
     { Physical: 1, Name: '3.3v', WPi: null, BCM: null },
     { Physical: 2, Name: '5v', WPi: null, BCM: null },
